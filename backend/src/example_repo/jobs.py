159f1a3ace6031ebb14c85a9f06c1cbac9368459
Luke Harris <6074660+luk707@users.noreply.github.com>
2024-03-12 16:02:41 +0000

    Fix db connection in test repo

--- backend/src/example_repo/jobs.py
+++ backend/src/example_repo/jobs.py
@@ -23,22 +23,27 @@ class JobsRepository:
 
     async def create(self, job: Job) -> Job:
         query = "INSERT INTO jobs (id, created_date, name) VALUES (:id, :created_date, :name)"
-        await self.database.execute(query, job.dict())
+        async with self.database as db:
+            await db.execute(query, job.model_dump())
         return job
 
     async def read_one(self, job_id: UUID) -> Optional[Job]:
         query = "SELECT id, created_date, name FROM jobs WHERE id = :id"
-        row = await self.database.fetch_one(query, {"id": job_id})
+        async with self.database as db:
+            row = await db.fetch_one(query, {"id": job_id})
         return parse_job(row) if row else None
 
-    async def read_many(self, job_ids: Optional[List[UUID]]) -> List[Job]:
+    async def read_many(self, job_ids: Optional[List[UUID]] = None) -> List[Job]:
         if job_ids:
             placeholders = ','.join([str(job_id) for job_id in job_ids])
             query = f"SELECT id, created_date, name FROM jobs WHERE id IN ({placeholders})"
-            rows = await self.database.fetch_all(query)
+            async with self.database as db:
+                rows = await db.fetch_all(query)
         else:
             query = "SELECT id, created_date, name FROM jobs"
-            rows = await self.database.fetch_all(query)
+
+            async with self.database as db:
+                rows = await db.fetch_all(query)
         return [parse_job(row) for row in rows]
 
     async def update_partial(self, job: Job) -> Optional[Job]:
@@ -46,9 +51,11 @@ class JobsRepository:
             [f"{key} = :{key}" for key in job.model_dump(exclude={"id"})])
         values = job.model_dump()
         query = f"UPDATE jobs SET {update_fields} WHERE id = :id"
-        await self.database.execute(query, values)
+        async with self.database as db:
+            await db.execute(query, values)
         return job
 
     async def delete(self, job_id: UUID) -> None:
         query = "DELETE FROM jobs WHERE id = :id"
-        await self.database.execute(query, {"id": job_id})
+        async with self.database as db:
+            await db.execute(query, {"id": job_id})
