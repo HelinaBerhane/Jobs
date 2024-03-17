f940ca378e7a42a7c58edbfdf948767987c8382e
Helina <helina.berhane@outlook.com>
2024-03-17 10:27:10 +0000

    Write a simple react app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,1 +1,5 @@
-console.log("Hello World");
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>Hello World</h1>);
