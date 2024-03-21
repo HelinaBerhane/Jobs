f176460e45fde9f21932ef4d66e60f4df74e786b
Helina <helina.berhane@outlook.com>
2024-03-21 16:21:31 +0000

    Move hello world to App.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello World</h1>);
+import App from "./App";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+    // Note: <StrictMode> helps to find common bugs in components early during development.
+    // See https://react.dev/reference/react/StrictMode
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
